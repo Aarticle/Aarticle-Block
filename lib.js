dadeabca3301001f1abcc4fc145973ea5ab1de5f
Elliot Briant Oliveira <elliotbriant@Elliots-MacBook-Air.local>
2018-04-24 15:11:48 -0700

    added the new protocol to lib

--- lib.js
+++ lib.js
@@ -2,12 +2,11 @@ const libp2p = require('libp2p')
 const TCP = require('libp2p-tcp')
 const PeerInfo = require('peer-info')
 const waterfall = require('async/waterfall')
+const parallel = require('async/parallel')
+const pull = require('pull-stream')
 
-// This MyBundle class is your libp2p bundle packed with TCP
 class MyBundle extends libp2p {
   constructor (peerInfo) {
-    // modules is a JS object that will describe the components
-    // we want for our libp2p bundle
     const modules = {
       transport: [new TCP()]
     }
@@ -15,33 +14,81 @@ class MyBundle extends libp2p {
   }
 }
 
-let node
+function createNode (callback) {
+  let node
 
-waterfall([
-  // First we create a PeerInfo object, which will pack the
-  // info about our peer. Creating a PeerInfo is an async
-  // operation because we use the WebCrypto API
-  // (yeei Universal JS)
-  (cb) => PeerInfo.create(cb),
-  (peerInfo, cb) => {
-    // To signall the addresses we want to be available, we use
-    // the multiaddr format, a self describable address
-    peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
-    // Now we can create a node with that PeerInfo object
-    node = new MyBundle(peerInfo)
-    // Last, we start the node!
-    node.start(cb)
-  }
-], (err) => {
+  waterfall([
+    (cb) => PeerInfo.create(cb),
+    (peerInfo, cb) => {
+      peerInfo.multiaddrs.add('/ip4/0.0.0.0/tcp/0')
+      node = new MyBundle(peerInfo)
+      node.start(cb)
+    }
+  ], (err) => callback(err, node))
+}
+
+parallel([
+  (cb) => createNode(cb),
+  (cb) => createNode(cb)
+], (err, nodes) => {
   if (err) { throw err }
 
-  // At this point the node has started
-  console.log('node has started (true/false):', node.isStarted())
-  // And we can print the now listening addresses.
-  // If you are familiar with TCP, you might have noticed
-  // that we specified the node to listen in 0.0.0.0 and port
-  // 0, which means "listen in any network interface and pick
-  // a port for me
-  console.log('listening on:')
-  node.peerInfo.multiaddrs.forEach((ma) => console.log(ma.toString()))
-});
+  const node1 = nodes[0]
+  const node2 = nodes[1]
+
+  // exact matching
+  node2.handle('/your-protocol', (protocol, conn) => {
+    pull(
+      conn,
+      pull.map((v) => v.toString()),
+      pull.log()
+    )
+  })
+
+  // semver matching
+  /*
+  node2.handle('/another-protocol/1.0.1', (protocol, conn) => {
+    pull(
+      conn,
+      pull.map((v) => v.toString()),
+      pull.log()
+    )
+  })
+  */
+
+  // custom func matching
+  /*
+  node2.handle('/custom-match-func', (protocol, conn) => {
+    pull(
+      conn,
+      pull.map((v) => v.toString()),
+      pull.log()
+    )
+  }, (myProtocol, requestedProtocol, callback) => {
+    if (myProtocol.indexOf(requestedProtocol)) {
+      callback(null, true)
+    } else {
+      callback(null, false)
+    }
+  })
+  */
+
+  node1.dialProtocol(node2.peerInfo, '/your-protocol', (err, conn) => {
+    if (err) { throw err }
+    pull(pull.values(['my own protocol, wow!']), conn)
+  })
+
+  /*
+  node1.dialProtocol(node2.peerInfo, '/another-protocol/1.0.0', (err, conn) => {
+    if (err) { throw err }
+    pull(pull.values(['semver me please']), conn)
+  })
+  */
+
+  /*
+  node1.dialProtocol(node2.peerInfo, '/custom-match-func/some-query', (err, conn) => {
+    if (err) { throw err }
+    pull(pull.values(['do I fall into your criteria?']), conn)
+  })
+  */
+})
